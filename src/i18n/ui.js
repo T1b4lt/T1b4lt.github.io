bb9df9c124fb212b66b8e9d1a83b33ed40c66bbe
T1b4lt <guillermosegfer@gmail.com>
2023-11-09 15:23:38 +0100

    localization and some assets

---
+++ src/i18n/ui.js
@@ -0,0 +1,17 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "lang.english": "English",
+    "lang.spanish": "Spanish",
+  },
+  es: {
+    "lang.english": "Inglés",
+    "lang.spanish": "Español",
+  },
+};
