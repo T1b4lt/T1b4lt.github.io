22875a9d17d748738cd5c0791562ecdd08a4b9e3
T1b4lt <guillermosegfer@gmail.com>
2023-11-11 18:54:16 +0100

    first release without blog

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -1,6 +1,6 @@
 export const languages = {
-  en: "English",
   es: "Español",
+  en: "English",
 };
 
 export const defaultLang = "en";
@@ -9,9 +9,18 @@ export const ui = {
   en: {
     "nav.home": "Home",
     "nav.blog": "Blog",
+    "404.title": "You are lost",
+    "404.button": "Go back home",
+    "footer.title": "Stay in touch!",
+    "footer.subtitle": "Follow me on social media for updates and news.",
   },
   es: {
     "nav.home": "Inicio",
     "nav.blog": "Blog",
+    "404.title": "Te has perdido",
+    "404.button": "Vuelve a casa",
+    "footer.title": "¡Mantente en contacto!",
+    "footer.subtitle":
+      "Sígueme en redes sociales para estar al tanto de las novedades.",
   },
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    /*
+    colors: {
+      viridian: "#4E937A",
+      pear: "#D0E562",
+      coolblue: "3E78B2",
+    },
+    */
   },
   plugins: [],
   darkMode: "class",
